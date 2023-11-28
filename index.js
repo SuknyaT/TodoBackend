d82ed8aaead28cd52e62641737d952301cccc372
SuknyaT <suknyat1995@gmail.com>
2023-11-28 17:05:28 +0530

    cors issue fix

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.use(
     extended: true,
   })
 );
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 // Start the Express server and listen on the specified port
 app.listen(port, () => {
